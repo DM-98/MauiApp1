9c83f3b08cfa91855e52db1ffe594de0b25a471f
Danny Møller <dannymoller@live.dk>
2023-08-11 17:20:22 +0200

    Initial Commit

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	private readonly HttpClient httpClient;
+	private readonly JsonSerializerOptions jsonSerializerOptions;
+
+	public MainPage(HttpClient httpClient)
+	{
+		InitializeComponent();
+
+		this.httpClient = httpClient;
+		this.httpClient.DefaultRequestHeaders.Add("X-Tunnel-Authorization", "tunnel <Your Tunnel Access Token>"); // if u use tunnel
+
+		jsonSerializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		try
+		{
+			HttpResponseMessage responseMessage = await httpClient.PostAsJsonAsync("WeatherForecast", new WeatherForecast
+			{
+				Date = DateOnly.FromDateTime(DateTime.Now.AddDays(1)),
+				TemperatureC = Random.Shared.Next(-20, 55),
+				Summary = "Test"
+			}, jsonSerializerOptions);
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				WeatherForecast weatherForecastResult = await responseMessage.Content.ReadFromJsonAsync<WeatherForecast>(jsonSerializerOptions);
+
+				await DisplayAlert("Success", $"Date: {weatherForecastResult.Date}, TemperatureC: {weatherForecastResult.TemperatureC}, Summary: {weatherForecastResult.Summary}", "OK");
+			}
+			else
+			{
+				await DisplayAlert("Error", responseMessage.ReasonPhrase, "OK");
+			}
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlert("Error", "Exception: " + ex.Message + " | Inner Exception: " + ex.InnerException, "OK");
+		}
+	}
+}
---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		MauiAppBuilder builder = MauiApp.CreateBuilder();
+
+		builder.UseMauiApp<App>();
+		builder.ConfigureFonts(fonts =>
+		{
+			fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+		});
+
+		builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddSingleton(httpClient => new HttpClient { BaseAddress = new Uri("https://<redacted>.euw.devtunnels.ms/") }); // Your API
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
